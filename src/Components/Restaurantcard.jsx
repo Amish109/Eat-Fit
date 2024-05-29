2f9cf261cac5be140971ec1a75657f997bb3a3d4
Amish109 <tiwariamish786@gmail.com>
2024-05-29 15:06:58 +0530

    Changes Restaurant card

--- src/Components/Restaurantcard.jsx
+++ src/Components/Restaurantcard.jsx
@@ -1,12 +1,13 @@
-const Restaurantcard=({areaName,avgRating,cuisines,name,sla})=>{
+import { HostUrl } from "../constants/restaurants"
+const Restaurantcard=({areaName,avgRating,cuisines,name,sla,cloudinaryImageId})=>{
     //imageURL , title,starRating,deliveryTime,cuisine,address
     return(
         <div className="Restaurantcard mb-4">
             <div className="image"  style={{height:"216px",display:"flex"}}>
-                <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e0839ff574213e6f35b3899ebf1fc597" width="90%"height="100%" style={{borderRadius:"26px"}} />
+                <img src={HostUrl+cloudinaryImageId} width="90%"height="100%" style={{borderRadius:"26px"}} />
             </div>
             <div className="content px-2 mt-2">
-                <h4>{name}</h4>
+                <h4 className="">{name}</h4>
                 <div className="d-flex justify-content-between"><span>⭐{avgRating}</span> <span>{sla.deliveryTime} mins</span></div>
                 <div className="cuisines" style={{whiteSpace:"nowrap",overflow:"hidden",textOverflow:"ellipsis"}}title={cuisines.join(",")}>{cuisines.join(",")}</div>
                 <p>{areaName}</p>
