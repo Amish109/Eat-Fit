d51bfbad2bea8f764f0358e5fe06cbc834dacf61
Amish109 <tiwariamish786@gmail.com>
2024-06-07 01:39:09 +0530

    Search Filter

--- src/Components/Cardcontainer.jsx
+++ src/Components/Cardcontainer.jsx
@@ -1,7 +1,10 @@
 import RestaurantCard from "./RestaurantCard";
-import { restaurantList } from "../constants/restaurants";
 import { useEffect, useState } from "react";
 const CardContainer = () => {
+  // let {card:{card:{gridElements:{infoWithStyle:{restaurants}}}}}=restaurantList[1]
+ const [storedData,setStoredData]=useState([]);
+ const [restaurantData,setRestaurantData]=useState([]);
+ const [textData,updateTextData]=useState("");
   const ApiCall =async()=>{
     const response = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.07480&lng=72.88560&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",{
      header:{
@@ -9,18 +12,22 @@ const CardContainer = () => {
      }
     })
     const data =await response.json();
-    console.log('ApiCallData',data)
+    console.log(data.data.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    setRestaurantData(data.data.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    setStoredData(data.data.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
   }
-  let {card:{card:{gridElements:{infoWithStyle:{restaurants}}}}}=restaurantList[1]
- const [restaurantData,setRestaurantData]=useState(restaurants)
   // let {card:{card:{gridElements:{infoWithStyle:{restaurants}}}}}=restaurantList[1];
   const filterData=()=>{
-    const filteredData=restaurants.filter((restaurant)=>{
+    const filteredData=restaurantData.filter((restaurant)=>{
       return restaurant.info.avgRating>=4.5
     })
     setRestaurantData(filteredData);
     console.log(filteredData)
 
+  }
+  const searchData=()=>{
+    const searchData=storedData.filter((element,index)=>element.info.name.toLowerCase().includes(textData.toLowerCase()));
+    setRestaurantData(searchData);
   }
   // ApiCall()
   useEffect(()=>{
@@ -29,8 +36,14 @@ const CardContainer = () => {
 
   return (
     // <div className="container "style={{display:"grid",gridTemplateColumns:"repeat(4,1fr)"}}>
-    <>
-      <button className="btn btn-primary btn-sm ms-2" onClick={filterData}>Top Rated</button>
+    <div>
+      <div className="container mb-3 d-flex justify-content-between">
+        <div>
+          <input type="text" value={textData} onChange={(event)=>{updateTextData(event.target.value)}} onKeyUp={searchData} placeholder="Search.." className="border border-dark p-2 col-md-8" style={{borderRadius:'20px',width:'500px'}} />
+          <button className="btn btn-primary btn-sm ms-2" onClick={searchData}>Search</button>
+        </div>
+        <button className="btn btn-primary btn-sm ms-2" onClick={filterData}>Top Rated</button>
+      </div>
       <div className="container d-flex flex-wrap gap-5 ">
         {
           // console.log(restaurants);
@@ -46,7 +59,7 @@ const CardContainer = () => {
           })
         }
       </div>
-    </>
+    </div>
   )
 }
 export default CardContainer
